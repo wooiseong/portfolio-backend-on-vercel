bde98275dda941a3c259c22cb93a2a5dd951df4b
Xiong <wooiseongseong96@gmail.com>
2024-12-01 16:32:03 +0800

    1

--- index.js
+++ index.js
@@ -23,7 +23,8 @@ const db = mysql.createPool({
   host: process.env.DB_HOST,
   user: process.env.DB_USERNAME,
   password: process.env.DB_PASSWORD,
-  database: process.env.DB_DBNAME
+  database: process.env.DB_DBNAME,
+  port: process.env.DB_PORT
 })
 
 //3.處理信息
