3e9963d43cb6daea43a458d2cfe6b055aabecfb4
Xiong <wooiseongseong96@gmail.com>
2024-12-01 20:01:48 +0800

    1

--- index.js
+++ index.js
@@ -3,8 +3,33 @@ const express = require('express')
 const app = express() 
 const PORT = 4000
 
-
 app.use(express.json())
+
+
+const allowCors = fn => async (req, res) => {
+  res.setHeader('Access-Control-Allow-Credentials', true)
+  res.setHeader('Access-Control-Allow-Origin', '*')
+  // another common pattern
+  // res.setHeader('Access-Control-Allow-Origin', req.headers.origin);
+  res.setHeader('Access-Control-Allow-Methods', 'GET,OPTIONS,PATCH,DELETE,POST,PUT')
+  res.setHeader(
+    'Access-Control-Allow-Headers',
+    'X-CSRF-Token, X-Requested-With, Accept, Accept-Version, Content-Length, Content-MD5, Content-Type, Date, X-Api-Version'
+  )
+  if (req.method === 'OPTIONS') {
+    res.status(200).end()
+    return
+  }
+  return await fn(req, res)
+}
+
+const handler = (req, res) => {
+  const d = new Date()
+  res.end(d.toString())
+}
+
+module.exports = allowCors(handler)
+
 
 //cors跨域模塊
 const cors = require('cors')
